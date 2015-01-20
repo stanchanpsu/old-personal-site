92515d4306d1870c130252d93c2e085a0ef85ee7
stanchanpsu <stanchanpsu@outlook.com>
2015-01-19 21:16:43 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,66 @@
-function playNote(note) {
-        document.getElementById(note).currentTime = 0;
-		document.getElementById(note).play();
-        setTimeout(function(){
-            document.getElementById(note).pause();
-        }, 1000);
+$(document).ready(function(){
+	smoothScroll();
+	activeLink();
+	email();
+})
+
+function email(){
+	$('#email').hide();
+	$("#show-email").on('click', function(e){
+       $('#email').slideToggle("fast");
+        });
 }
 
-$(document).ready(function(){
-    $('.key').click(function(){
-        // console.log(event.target.id);
-        // var $audio = $("#"+ event.target.id + "Note").get(0).play()
-        // $("#"+ event.target.id + "Note").get(0).delay(1000).get(0).pause();
-        // console.log($audio);
-        playNote(event.target.id + "Note");
-    });
-});
+function smoothScroll(){
+	$('a[href*=#]').on('click',function (e) {
+		e.preventDefault();
+
+		var target = this.hash;
+		var $target = $(target);
+
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top
+		}, 250, 'swing', function () {
+			window.location.hash = target;
+		});
+	});
+}
+
+function activeLink(){
+	window.onscroll = function (event) {
+		$('a[href*=#]').each(function () {
+			$(this).removeClass('active');
+	  })
+
+		var top = $(window).scrollTop();
+
+		var home = $('#home').offset().top -2;
+		var about = $('#about').offset().top -2;
+		var projects = $('#projects').offset().top -2;
+		var blog = $('#blog').offset().top -2;
+		var contact = $('#contact').offset().top -2;
+
+		var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
+
+		console.log(scrollBottom)
+
+		if(top < about){
+			$('a[href="#home"]').addClass('active');
+		}
+		else if(top < gallery){
+			$('a[href="#about"]').addClass('active');
+		}
+		else if(top < projects){
+			$('a[href="#gallery"]').addClass('active');
+		}
+		else if(top < blog){
+			$('a[href="#projects"]').addClass('active');
+		}
+		else if(top < contact && scrollBottom >= 2){
+			$('a[href="#blog"]').addClass('active');
+		}
+		else{
+			$('a[href="#contact"]').addClass('active');
+		}
+	}
+}
